78940a84c7a971cacdc6ad5f0af7806867bb211a
ahmadmuqri0 <muqrisuhaimi25@gmail.com>
2025-10-26 21:35:11 +0800

    feat: Add dashboard page

--- src/router.ts
+++ src/router.ts
@@ -1,3 +1,8 @@
 import { createRouter } from "sv-router";
+import Dashboard from "@/pages/dashboard.svelte";
+import AppLayout from "$lib/layouts/app-layout.svelte";
 
-export const { p, navigate, isActive, route } = createRouter({});
+export const { p, navigate, isActive, route } = createRouter({
+  "/": Dashboard,
+  layout: AppLayout,
+});
